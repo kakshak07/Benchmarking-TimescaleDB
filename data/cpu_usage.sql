37a7c902bf531c7ecaaa018d07df784173709348
KAKSHAK <kakshak@KAKSHAKs-MBP.cable.rcn.com>
2024-10-13 23:27:59 -0400

    removed entire code repo

--- data/cpu_usage.sql
+++
@@ -1,8 +0,0 @@
-CREATE DATABASE homework;
-CREATE EXTENSION IF NOT EXISTS timescaledb;
-CREATE TABLE cpu_usage(
-  ts    TIMESTAMPTZ,
-  host  TEXT,
-  usage DOUBLE PRECISION
-);
-SELECT create_hypertable('cpu_usage', 'ts');
